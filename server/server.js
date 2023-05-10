86e7e91a0d0b28def497cd1c2b67a77a9d5d50e4
averrani <aris.issad@gmail.com>
2023-05-10 09:05:19 +0200

    bug corr

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const apiServ = require('./pres/apiPres');
-const consolePres = require('./pres/consolePres');
+//const consolePres = require('./pres/consolePres');
 const port = 3001;
 
 function main(){
   //pour lancer l'api 
-  //apiServ.start(port);
+  apiServ.start(port);
 
   //pour lancer la pres console
-  consolePres.start();
+  //consolePres.start();
 
 }
 
